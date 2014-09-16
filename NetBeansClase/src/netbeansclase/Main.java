abb1c2139b5911caf7c09993b2a455407f951813
campusaula <nestor.martinez@campusaula.com>
2014-09-16 09:36:17 +0200

    Añado método saludar.
    Cambio Hola del main por Hola Mundo.

--- NetBeansClase/src/netbeansclase/Main.java
+++ NetBeansClase/src/netbeansclase/Main.java
@@ -15,9 +15,14 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+     
+    public void saludar(){
+        System.out.println("Hola");
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola");
+        System.out.println("Hola Mundo...!!!");
     }
     
 }
