8dba2b41066f1f3bd1fb0531673278a7d7808655
nestor <nestor@nestor>
2014-09-16 09:40:18 +0200

    Añado método saludar y comento en main.

--- NetBeansClase/src/netbeansclase/Main.java
+++ NetBeansClase/src/netbeansclase/Main.java
@@ -15,9 +15,14 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    public static void saludar(){
+        System.out.println("Hola");
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
+        //saludar();
     }
     
 }
